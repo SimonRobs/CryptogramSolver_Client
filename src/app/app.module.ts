6dda495856b150a89182658e850e70737730cffe
Simon Robatto <simon.robatto-simard@polymtl.ca>
2022-08-12 19:29:50 -0400

    Added heroku url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { EncryptedLetterInputComponent } from './components/encrypted-letter-input/encrypted-letter-input.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { APP_URL, PORT } from './constants';
 
-const config: SocketIoConfig = { url: `${APP_URL}:${PORT}`, options: {} };
+const config: SocketIoConfig = { url: environment.serverUrl || `${APP_URL}:${PORT}`, options: {} };
 AngularSvgIconModule.forRoot();
 
 @NgModule({
